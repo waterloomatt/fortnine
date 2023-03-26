064771f05b745b4a45aa5ad140cb140cd96230a9
Matt Skelton <waterloomatt@gmail.com>
2023-03-26 12:37:12 -0400

    Add relationships

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
