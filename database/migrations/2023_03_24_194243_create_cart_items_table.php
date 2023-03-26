a2ff4e5e308aaa0502cdbf158f6db622caa74d56
Matt Skelton <waterloomatt@gmail.com>
2023-03-26 12:31:53 -0400

    Update foreign keys

--- database/migrations/2023_03_24_194243_create_cart_items_table.php
+++ database/migrations/2023_03_24_194243_create_cart_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,7 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_id')->constrained();
             $table->foreignId('product_id')->constrained();
             $table->integer('quantity');
             $table->timestamps();
