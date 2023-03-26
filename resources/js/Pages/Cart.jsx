e6b17139a69f54a3cc2c8bade264ecdf9d440f3b
Matt Skelton <waterloomatt@gmail.com>
2023-03-26 18:13:57 -0400

    Update checkout text on click

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -11,6 +11,8 @@ export default function Cart({}) {
         constants: { tax },
     } = usePage().props;
 
+    const [checkoutText, setCheckoutText] = useState('Checkout');
+
     const [itemTotals, setItemTotals] = useState(
         cart.map((item) => {
             return {
@@ -131,9 +133,10 @@ export default function Cart({}) {
                     <div className="mt-6">
                         <a
                             href="#"
+                            onClick={() => setCheckoutText('Checkout is not implemented')}
                             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
-                            Checkout
+                            {checkoutText}
                         </a>
                     </div>
                     <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
