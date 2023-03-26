ceabbbf99cb71c5e0b6ced1df3084c8bbc0014a8
Matt Skelton <waterloomatt@gmail.com>
2023-03-26 12:38:19 -0400

    Add routes and controller

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Facades\Cart;
+use App\Http\Requests\AddCartItemRequest;
+use App\Http\Requests\RemoveCartItemRequest;
+use App\Http\Requests\UpdateCartItemRequest;
+use App\Models\Product;
+use Inertia\Inertia;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Shop', [
+            'products' => Product::paginate(config('custom.page_size')),
+        ]);
+    }
+
+    public function view(Product $product)
+    {
+        return Inertia::render('Product', [
+            'product' => $product,
+        ]);
+    }
+
+    public function cart()
+    {
+        return Inertia::render('Cart', [
+            'cart' => Cart::getItems(),
+        ]);
+    }
+
+    public function add(AddCartItemRequest $request)
+    {
+        $validated = $request->validated();
+
+        Cart::add($validated['product_id'], $validated['quantity']);
+
+        return back()->withInput();
+    }
+
+    public function remove(RemoveCartItemRequest $request)
+    {
+        $validated = $request->validated();
+
+        Cart::remove($validated['product_id']);
+    }
+
+    public function update(UpdateCartItemRequest $request)
+    {
+        $validated = $request->validated();
+
+        Cart::update($validated['product_id'], $validated['quantity']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,23 +14,12 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [ShopController::class, 'index'])->name('shop');
+Route::get('/product/{product}', [ShopController::class, 'view'])->name('product');
+Route::post('/cart', [ShopController::class, 'add'])->name('add');
+Route::post('/clear', [ShopController::class, 'clear'])->name('clear');
+Route::post('/update', [ShopController::class, 'update'])->name('update');
+Route::post('/remove', [ShopController::class, 'remove'])->name('remove');
+Route::get('/cart', [ShopController::class, 'cart'])->name('cart');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
