61e42c00b57a5af7d44a7376b37ee48c714fc50f
Matt Skelton <waterloomatt@gmail.com>
2023-03-28 11:33:15 -0400

    Changes for native select element

--- resources/js/Components/AddToCartForm.jsx
+++ resources/js/Components/AddToCartForm.jsx
@@ -36,7 +36,7 @@ export default function AddToCartForm({ product }) {
             },
             {
                 preserveScroll: true,
-                onFinish: (visit) => {
+                onFinish: () => {
                     setIsOpen(true);
                     setQuantity(1);
                 },
@@ -72,11 +72,9 @@ export default function AddToCartForm({ product }) {
                         <select
                             className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             key={`quantity_select__${quantity}`}
-                            defaultValue={options.find(
-                                ({ value }) => value === quantity
-                            )}
-                            onChange={(selected) => setQuantity(selected.value)}
+                            onChange={(e) => setQuantity(e.target.value)}
                             placeholder={"Quantity"}
+                            value={quantity}
                         >
                             {map(options, (option) => {
                                 return (
--- resources/js/Components/CartSummaryItem.jsx
+++ resources/js/Components/CartSummaryItem.jsx
@@ -15,8 +15,8 @@ export default function CartSummaryItem({
     const [quantity, setQuantity] = useState(item.quantity);
 
     const handleQuantityChange = (e) => {
-        setQuantity(+e.value);
-        quantityHandler(item.id, item.price, +e.value);
+        setQuantity(+e.target.value);
+        quantityHandler(item.id, item.price, +e.target.value);
     };
 
     const handleRemoveItem = () => {
@@ -58,9 +58,7 @@ export default function CartSummaryItem({
                     <div className="flex flex-1 items-end justify-between text-sm">
                         <select
                             className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5"
-                            defaultValue={options.find(
-                                ({ value }) => value === quantity
-                            )}
+                            value={quantity}
                             onChange={handleQuantityChange}
                             placeholder={"Quantity"}
                         >
