59f42b515a7f5fb0a8676d84ee9fade3072df7ae
Matt Skelton <waterloomatt@gmail.com>
2023-04-12 15:55:16 -0400

    Revert

--- resources/js/Components/ProductCard.jsx
+++ resources/js/Components/ProductCard.jsx
@@ -13,7 +13,7 @@ export default function ProductCard({ product }) {
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">
-                    $99.99
+                    ${product.price}
                 </p>
             </div>
         </Link>
